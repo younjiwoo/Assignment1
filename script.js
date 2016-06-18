b42eb919b5f94f7e5eca16c7f3d785524858f430
Younji <younjiwoo@gmail.com>
2016-06-18 15:36:39 -0700

    changed the title

---
+++ script.js
@@ -0,0 +1,142 @@
+// step 1
+
+//var someMonth;
+//function currentMonth();
+//CURRENT_MONTH;
+//var summerMonths;
+//function myLibrary();
+
+// step 2
+
+23532;
+"this is github";
+true;
+null;
+
+// step 3
+
+var myName = "Younji" + " Woo";
+var myAge = 30;
+
+// step 4 
+
+//var strFirstName,
+//    strLastName,
+//    strAddress,
+//    strCity,
+//    strState,
+//    strZipCode,
+//    intYourAge,
+//    strReferralSource,
+//    blnMayWeContactYou;
+
+// step 5
+
+var strFirstName;
+strFirstName = "Younji";
+
+var strLastName = "Woo";
+
+var strAddress = "La Paz", 
+    strCity = "San Diego",
+    strState = "California",
+    strZipCode = "92121",
+    intYourAge = "27",
+    strReferralSource = "friend",
+    blnMayWeContactYou = false;
+
+// step 6
+
+var firstVar = 3048 + "string";
+console.log(firstVar);
+
+var secVar = true + "string";
+console.log(secVar);
+
+var thirdVar = 4938 + false;
+console.log(thirdVar);
+
+// step 7
+
+var make = "Chevy";
+console.log("The car is a " + make + " " + model + ".");
+var model = "car";
+
+// step 8
+
+//It's not valid. 
+var someString = 'Who once said, "Only two things are infinte, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString);
+
+// step 9
+
+var coffee = null;
+var tea = undefined;
+
+// step 10
+
+console.log(typeof "younjiwoo");
+console.log(typeof 394094);
+console.log(typeof false);
+console.log(typeof {});
+console.log(typeof undefined);
+
+// step 11
+//
+//alert("Hello" + " Younji Woo" + ", " + "welcome to the JavaScript class!");
+//
+//// step 12
+//
+var myNameIs = "Younji Woo";
+//alert("Hello " + myNameIs + ", welcome to the JavaScript class!");
+
+// step 13
+
+var course= "JavaScript";
+//alert("Hello " + myNameIs + ", welcome to the " + course + " class!");
+
+// step 14
+
+//alert("Hello " + myNameIs + ". \nWelcome to the " + course + " class!");
+
+// step 15
+//var myNameIs = prompt("What's your name?");
+
+// step 16
+//var course = prompt("What course are you taking?");
+
+// step 17
+var x = 10;
+var y = 20;
+console.log(x + y);
+
+// step 18
+var x = 20;
+x += 20;
+console.log(x);
+
+// step 19
+var x = 20;
+x *= 5;
+console.log(x);
+
+// step 20
+var x = 20 % 3;
+x /= 1;
+console.log(x);
+
+// step 21
+console.log(50 > 39 === true);
+
+// step 22
+console.log(50 < 2 !== false);
+
+// step 23
+var widget = new Object();
+console.log(typeof widget);
+
+// step 24
+console.log(widget instanceof Object);
+
+// step 25
+console.log(widget instanceof String);
